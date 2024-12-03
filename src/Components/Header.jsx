9a88658c9e482d32e1ec686bd0c0a68b8c95b28d
Priya Acharjee <priyaacharjee2001@gmail.com>
2024-12-04 00:48:37 +0530

    updating

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import { HiMenu, HiX } from 'react-icons/hi';
 
@@ -38,7 +38,7 @@ export default function Header() {
           to="contact"
           smooth={true}
           duration={500}
-          className="bg-blue-700 text-white px-4 py-2 rounded-full hover:bg-pink-800 cursor-pointer"
+          className="bg-blue-700 text-white px-4 py-2 rounded-sm hover:bg-pink-800 cursor-pointer"
         >
           Contact
         </ScrollLink>
